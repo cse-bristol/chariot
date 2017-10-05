bdfee5396f0430a3885ef224d5a4d5c15c335118
trickyBytes <richard.tiffin@akoolla.com>
2017-10-05 13:02:01 +0100

    Send notification only if one hasn't been sent for a certain period + some work on the emailing of notifications

--- src/app/deployments/receivers.py
+++ src/app/deployments/receivers.py
@@ -3,6 +3,7 @@
 from chariot.influx import influx
 from deployments.models import Deployment, DeploymentSensor
 from datetime import timedelta, datetime
+import smtplib
 
 #@reciever(sensor_reading_recieved)
 def receive_notification(sender, deployment_pk, sensor, temp):
@@ -29,11 +30,13 @@ def receive_notification(sender, deployment_pk, sensor, temp):
 
 def _send_notifications(deployment,sensor,value):
     if _ok_to_send_notification(sensor.last_notification_sent):
+        _send_notification_email(deployment,sensor,"XXX")
         sensor.last_notification_sent = datetime.today()
         sensor.save()
 
 def _ok_to_send_notification(last_notification_sent):
     an_hour = timedelta(seconds=60*60)
+    an_hour = timedelta(seconds=20) ##TODO REMOVE BEFORE COMMIT
     if last_notification_sent is not None and datetime.now(last_notification_sent.tzinfo) < (last_notification_sent + an_hour):
         return False
     else:
@@ -51,4 +54,15 @@ def _store_notification(deployment,sensor_id,value):
         }
     }
     influx.write_points([notification])
+
+
+def _send_notification_email(deployment,sensor,notification_type):
+    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    server.ehlo()
+    #server.starttls()
+    server.login("membershipmanagerdemo@gmail.com", "PASSWORD")
+    
+    msg = "\nHello!"
+    server.sendmail("you@gmail.com", "richard.tiffin@cse.org.uk", msg)
+    server.quit()
     
